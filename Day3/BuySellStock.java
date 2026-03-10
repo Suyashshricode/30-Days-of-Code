bafbb2fa97ad980348459b7de7bd71ab9fc08a55
Suyash Shrivastava <suyashshrivastavaoff26@gmail.com>
2026-03-10 10:40:50 +0530

    Day3 First question - Best time to buy and sell stocks

---
+++ Day3/BuySellStock.java
@@ -0,0 +1,36 @@
+package Day3;
+import java.util.*;
+
+public class BuySellStock {
+
+  public int bestTimeToBuy(int n , int [] prices)
+  {
+    int bestBuy = Integer.MAX_VALUE;
+    int maxProfit = 0;
+
+    for(int i=0; i<n; i++)
+    {
+      bestBuy = Math.min(bestBuy , prices[i]);
+      if(bestBuy<prices[i])
+      {
+        int profit = prices[i] - bestBuy;
+        maxProfit = Math.max(profit , maxProfit);
+      }
+    }
+    return maxProfit;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int prices[] = new int[n];
+    for(int i=0; i<n; i++)
+    {
+      prices[i] = sc.nextInt();
+    }
+
+    BuySellStock obj = new BuySellStock();
+    System.out.println(obj.bestTimeToBuy(n, prices));
+  }
+}
