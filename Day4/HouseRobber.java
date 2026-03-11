f03444096b613c2925edea7d0ec744c276342ad1
Suyash Shrivastava <suyashshrivastavaoff26@gmail.com>
2026-03-11 10:30:21 +0530

    Day4 Second question- LC-198 House robber

---
+++ Day4/HouseRobber.java
@@ -0,0 +1,34 @@
+package Day4;
+import java.util.*;
+public class HouseRobber {
+  public static int rob(int[]nums , int index)
+  {
+
+    if(index == 0)
+    {
+      return nums[0];
+    }
+
+    if(index<0)
+    {
+      return 0;
+    }
+    
+    int pick = nums[index] + rob(nums, index-2);
+    int notPick = 0 + rob(nums, index-1);
+
+    return Math.max(pick , notPick);
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int nums[] = new int[n];
+    for(int i=0; i<n; i++)
+    {
+      nums[i] = sc.nextInt();
+    }
+
+    System.out.println(rob(nums, n-1));
+  }
+}
